31dcef91ee1a49a3e5e63959667001d965f4bc20
chungSungMin <taky0315@naver.com>
2024-04-12 09:39:40 +0900

    dummy data setting

--- main.py
+++ main.py
@@ -5,59 +5,53 @@ from pydantic import BaseModel
 import os
 import logging
 
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
+from app.router.geport import router as geport_router
+from app.router.igeport import router as igeport_router
 
-mongodb_url = os.getenv("MONGODB_URL", "mongodb://admin:1234@localhost:27017/admin")
-logger.info(f"MONGODB_URL: {mongodb_url}")
 
 app = FastAPI(root_path="/api")
-
-# MongoDB 연결
-client = MongoClient(mongodb_url)
-db = client["admin"]  # MongoDB 연결 시 사용한 데이터베이스
-
-# Pydantic 모델 정의
-class Item(BaseModel):
-    name: str
-    description: Union[str, None] = None
-    price: float
-    tax: Union[float, None] = None
-
-class AnyData(BaseModel):
-    data: dict
-
-@app.post("/insert-data/")
-async def insert_data(data: AnyData):
-    if data.data is None:
-        raise HTTPException(status_code=400, detail="No data provided")
-    result = db.any_collection.insert_one(data.data)  # 'any_collection'을 실제 컬렉션 이름으로 변경하세요
-    return {"inserted_id": str(result.inserted_id)}
-
+app.include_router(geport_router)
+app.include_router(igeport_router)
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
 
-# MongoDB에 아이템 추가
-@app.post("/items/")
-def create_item(item: Item):
-    item_dict = item.dict()
-    if item.tax:
-        price_with_tax = item.price + item.tax
-        item_dict.update({"price_with_tax": price_with_tax})
-    db.items.insert_one(item_dict)
-    return item_dict
 
-# MongoDB에서 아이템 조회
-@app.get("/items/")
-def read_items():
-    items = list(db.items.find())
-    # MongoDB의 ObjectId를 문자열로 변환
-    for item in items:
-        item["_id"] = str(item["_id"])
-    return items
+# @app.get("/generate_branding_report/{encrypted_id}", response_model=dict)
+# async def generate_branding_report(encrypted_id: str):
+#     # MongoDB에서 사용자 정보 조회
+#     user_data = read_user_service(encrypted_id)
+#     if not user_data:
+#         raise HTTPException(status_code=404, detail="User not found")
+
+#     # ChatGPT를 사용하여 개인 브랜딩 보고서 생성
+#     response = ask_chatgpt(user_data)
+#     return {"ChatGPT Response": response}
+
+
+
+
+# @app.get("/items/{item_id}")
+# def read_item(item_id: int, q: Union[str, None] = None):
+#     return {"item_id": item_id, "q": q}
+
+# # MongoDB에 아이템 추가
+# @app.post("/items/")
+# def create_item(item: Item):
+#     item_dict = item.dict()
+#     if item.tax:
+#         price_with_tax = item.price + item.tax
+#         item_dict.update({"price_with_tax": price_with_tax})
+#     db.items.insert_one(item_dict)
+#     return item_dict
+
+# # MongoDB에서 아이템 조회
+# @app.get("/items/")
+# def read_items():
+#     items = list(db.items.find())
+#     for item in items:
+#         item["_id"] = str(item["_id"])
+#     return items
+
